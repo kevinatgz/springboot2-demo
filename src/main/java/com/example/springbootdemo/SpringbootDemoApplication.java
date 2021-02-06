b243cad313c1f56a594156fca914e0807061af35
panghaiming <kevinatgz@qq.com>
2021-02-06 15:57:51 +0800

    activemq

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -1,9 +1,17 @@
 package com.example.springbootdemo;
 
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableAdminServer
+@EnableJms    //启动消息队列
+@EnableScheduling
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/example/springbootdemo/SpringbootDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.service.ActivityService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootDemoApplicationTests {
 
+	@Autowired
+	ActivityService activityService;
 	@Test
 	void contextLoads() {
+			activityService.getOne();
 	}
 
 }
